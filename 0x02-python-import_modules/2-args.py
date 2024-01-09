91433db51153499467933bd005be2f8952a1d686
IO <issam_ouzali@outlook.fr>
2024-01-09 03:50:57 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
 import sys
 
 if __name__ != "__main__":
     exit()
 
-title = " argument"
-countArgs = len(sys.argv) - 1
-title = str(countArgs) + title
-
-if countArgs == 0:
-    title += "s."
-elif countArgs == 1:
-    title += ":"
+argStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argStr += 's.'
+elif argc == 1:
+    argStr += ':'
 else:
-    title += "s:"
+    argStr += 's:'
+print(argStr.format(argc))
 
-print(title)
-for i in range(1, countArgs+1):
-    print(f"{i} : {sys.argv[i]}")
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
+    i += 1
