6ca90d494088d14ce8e210d65faf0ca16456058f
IO <issam_ouzali@outlook.fr>
2024-01-09 08:59:41 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list = []):
     nCount = len(my_list)
     for i in range(0, nCount):
         print(f"{my_list[i]}")
-
-a = [10,2,3]
-print_list_integer(a)
-
-
