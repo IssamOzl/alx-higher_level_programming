627c97f554f2104a34c829f743907c404d9e2917
IO <issam_ouzali@outlook.fr>
2024-01-23 11:56:28 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+nbprint = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end = "")
+            nbprint += 1
+        except EXP:
+            print(EXP.args)
+    print()
+    return nbprint
