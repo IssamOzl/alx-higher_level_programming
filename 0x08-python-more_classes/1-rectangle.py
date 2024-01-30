0042d98b94d3c791a1d2931cad888703ba4577f3
IO <issam_ouzali@outlook.fr>
2024-01-30 10:29:15 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+
+    def __init__(self,width=0,height=0):
+        self.width = width
+        self.height = height
+        
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self,width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = width
+
+    @property
+    def height(self):
+        self.__height = height
+    @height.setter
+    def height(self,height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0 :
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        
