f47e02efdca1eaeaacfd1edc70f9dcab25120bdd
IO <issam_ouzali@outlook.fr>
2024-01-31 05:39:46 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,4 +6,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError(essorMsg.format("first_name"))
     if type(last_name) != str:
         raise TypeError(essorMsg.format("last_name"))
-    return print("My name is {} {}".format(first_name,last_name))
+    return print("My name is {} {}".format(first_name, last_name))
