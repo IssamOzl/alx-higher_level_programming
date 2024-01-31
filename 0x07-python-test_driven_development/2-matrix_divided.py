35a9a714c21c4f97509075e1bf8c9075f479b46b
IO <issam_ouzali@outlook.fr>
2024-01-31 05:35:36 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,30 +3,24 @@
 
 
 def matrix_divided(matrix, div):
-    """
-    function that divides all elements of a matrix.
-    matrix must be a list of lists of integers or floats
-    Each row of the matrix must be of the same size
-    div must be a number (integer or float)
-    """
-
-    mtTypeError = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(div) not in (int, float):
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(errorMessage)
+    if not isinstance(matrix, list):
+        raise TypeError(errorMessage)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(errorMessage)
+        for item in lists:
+            if not isinstance(item, int) and not isinstance(item, float):
+                raise TypeError(errorMessage)
+    for lists in matrix:
+        if len(lists) == 0:
+            raise TypeError(errorMessage)
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) != list or len(matrix) == 0:
-        raise TypeError(mtTypeError)
-    for i in range(len(matrix)):
-        if type(matrix[i]) != list or len(matrix[i]) == 0:
-            raise TypeError(mtTypeError)
-        if i == 0:
-            initSize = len(matrix[0])
-        if len(matrix[i]) != initSize:
-            raise TypeError("Each row of the matrix must have the same size")
-        for j in range(len(matrix[i])):
-            if type(matrix[i][j]) not in (int, float):
-                raise TypeError(mtTypeError)
-            matrix[i][j] = round(matrix[i][j] / div, 2)
-
-    return matrix
+    return [[round(item / div, 2) for item in lists] for lists in matrix]
