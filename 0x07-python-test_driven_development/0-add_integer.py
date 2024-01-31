afc02acbf31c7a731e3be11c4cf5a1d891f939cc
IO <issam_ouzali@outlook.fr>
2024-01-31 05:14:19 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+""" 0-add_integer module """
+
 
 def add_integer(a, b=98):
+    """
+    function that adds 2 integers
+    a and b must be integers or floats, otherwise raise a TypeError exception
+    with the message a must be an integer or b must be an integer
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
