6e0413e1fd0b76b007f52f9cb6dbd2335e5ab263
IO <issam_ouzali@outlook.fr>
2024-01-31 05:48:51 -0800

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" 4-print_square module """
+
 
 def print_square(size):
+    """ function that prints a square with the character # """
     my_char = "#"
     if type(size) != int:
         raise TypeError("size must be an integer")
@@ -8,5 +11,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            print(my_char,end="")
+            print(my_char, end="")
         print()
