3ae11f412c028124629b3ad4da321971dd1c03a6
IO <issam_ouzali@outlook.fr>
2024-01-31 05:53:10 -0800

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+""" 5-text_indentation module """
+
 
 def text_indentation(text):
+    """ function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
     my_list = []
     just_jumped = True
-    
+
     if type(text) != str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
-        if (just_jumped == True and text[i] != ' ') or just_jumped == False:
-            print(text[i],end="")
+        if (just_jumped is True and text[i] != ' ') or just_jumped is False:
+            print(text[i], end="")
             just_jumped = False
-        if text[i] in (".","?",":"):
+        if text[i] in (".", "?", ":"):
             print()
             print()
             just_jumped = True
