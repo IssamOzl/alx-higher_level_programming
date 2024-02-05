039559b36656472e3494c97a87ba33238c1d08ef
IO <issam_ouzali@outlook.fr>
2024-02-05 05:05:15 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+""" 0-lookup module """
 
 
 def lookup(obj):
+    """function that returns the list of available attributes
+    nd methods of an object """
     return dir(obj)
