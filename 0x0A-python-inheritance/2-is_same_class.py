5980fe48a9d614a5f3d95dadee1a07ba6b10098d
IO <issam_ouzali@outlook.fr>
2024-02-05 05:30:41 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" 2-is_same_class module """
+
+def is_same_class(obj, a_class):
+    """ check if the object is exactly an instance of a class"""
+    return type(obj) is a_class
