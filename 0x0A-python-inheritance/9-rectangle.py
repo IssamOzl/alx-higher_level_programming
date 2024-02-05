4a00f75ab4848a19e1f5295a6a458fc3d672381e
IO <issam_ouzali@outlook.fr>
2024-02-05 07:17:54 -0800

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,17 +3,17 @@
 Contains the class BaseGeometry and subclass Rectangle
 """
 
-BaseGeometry = __import__('8-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
     def __init__(self, width, height):
         """instantiation of the rectangle"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
 
     def area(self):
         """ area = l x w """
