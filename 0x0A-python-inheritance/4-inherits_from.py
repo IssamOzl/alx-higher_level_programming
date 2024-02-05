4c41cb6097392b0edcc3ead41d7bc7c0303b6c84
IO <issam_ouzali@outlook.fr>
2024-02-05 06:08:37 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-4-inherits_from module
+Contains the inherits_from function
 """
 
 
 def inherits_from(obj, a_class):
-    """ True if obj is a subclass of a_class, otherwise False """
+    """returns true if obj is a subclass of a_class, otherwise false"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
