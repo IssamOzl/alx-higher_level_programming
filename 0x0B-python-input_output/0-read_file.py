7917e5fd1cddaa4b6f2fc32cc7c1b3a548d211ad
IO <issam_ouzali@outlook.fr>
2024-02-07 00:42:25 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """ eads a text file (UTF8) and prints it to stdout """
     with open(filename, "r", encoding="UTF-8") as my_file:
         print(my_file.read(), end="")
