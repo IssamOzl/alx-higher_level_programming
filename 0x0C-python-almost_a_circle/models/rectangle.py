a8f4077c13c4184fee37c41a3787a04381529871
IO <issam_ouzali@outlook.fr>
2024-02-14 06:17:01 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    '''A Rectangle class '''
+    '''A Rectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor """
+        '''Constructor.'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,42 +16,46 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''Width of this rectangle.'''
         return self.__width
 
     @width.setter
-    def width(self, val):
-        self.validate_integer("width", val, False)
-        self.__width = val
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
 
     @property
     def height(self):
+        '''Height of this rectangle.'''
         return self.__height
 
     @height.setter
-    def height(self, val):
-        self.validate_integer("height", val, False)
-        self.__height = val
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
 
     @property
     def x(self):
+        '''x of this rectangle.'''
         return self.__x
 
     @x.setter
-    def x(self, val):
-        self.validate_integer("x", val)
-        self.__x = val
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
 
     @property
     def y(self):
+        '''y of this rectangle.'''
         return self.__y
 
     @y.setter
-    def y(self, val):
-        self.validate_integer("y", val)
-        self.__y = val
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
 
     def validate_integer(self, name, value, eq=True):
-        ''' Method for validating the values '''
+        '''Method for validating the value.'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
@@ -95,7 +99,7 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
-            
+
     def to_dictionary(self):
         '''Returns dictionary representation of this class.'''
         return {"id": self.id, "width": self.__width, "height": self.__height,
