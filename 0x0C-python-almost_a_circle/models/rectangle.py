a11de74c620e38b719c01f25a09d5f396a7d16f2
IO <issam_ouzali@outlook.fr>
2024-02-15 12:22:16 -0800

    add rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Parameters:
+        Params:
             width(int): width of rectangle
             height(int): height
             x(int): co-ordinate of the rectangle
