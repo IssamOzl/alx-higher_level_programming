6d3608208f5e8d5f7f768185acb8b7b481316f2e
IO <issam_ouzali@outlook.fr>
2024-02-15 12:13:01 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,106 +1,190 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+"""
+Rectangle that inherits from Base
+"""
+
+
 from models.base import Base
+import json
 
 
 class Rectangle(Base):
-    '''A Rectangle class.'''
-
+    """
+    __init__ is used for initialization and acts as a class constructor
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
+        """
+        Parameters:
+            width(int): width of rectangle
+            height(int): height
+            x(int): co-ordinate of the rectangle
+            y(int): co-ordinate of the rectangle
+            id(int): identifier of thr rectangle
+        """
+        self.validator('width', width)
+        self.validator('height', height)
+        self.validator('y', y)
+        self.validator('x', x)
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
+        """
+        acts as a validator
+        """
+    def validator(self, attribute_name, value):
+        """
+        validates the attributes
+        """
+        if not isinstance(value, int):
+            raise TypeError(f'{attribute_name} must be an integer')
+        if attribute_name in {'width', 'height'} and value <=0:
+            raise ValueError(f'{attribute_name} must be > 0')
+        if attribute_name in {'x', 'y'} and value < 0:
+            raise ValueError(f'{attribute_name} must be >= 0')
+        setattr(self, f'_{type(self).__name__}__{attribute_name}', value)
+        """
+        getter to retrieve the privatewidth instance width variable
+        """
     @property
     def width(self):
-        '''Width of this rectangle.'''
+        """
+        retrieve instaance attribute
+        """
         return self.__width
-
+    """
+    setter for width
+    """
     @width.setter
     def width(self, value):
-        self.validate_integer("width", value, False)
-        self.__width = value
-
+        self.validator('width', value)
+    """
+    getter to retrieve thr private height instance
+    """
     @property
     def height(self):
-        '''Height of this rectangle.'''
+        """
+        retrieve thr height instance attribute
+        """
         return self.__height
-
+    """
+    setter for height
+    """
     @height.setter
     def height(self, value):
-        self.validate_integer("height", value, False)
-        self.__height = value
-
+        """
+        sets the private instance height
+        """
+        self.validator('height', value)
+    """
+    getter for private instace x
+    """
     @property
     def x(self):
-        '''x of this rectangle.'''
+        """
+        retrieves the private instace x
+        """
         return self.__x
-
+    """
+    setter for x
+    """
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
-        self.__x = value
-
+        """
+        sets the private instance x to value
+        """
+        self.validator('x', value)
+        """
+        getter for the private instance y
+        """
     @property
     def y(self):
-        '''y of this rectangle.'''
+        """
+        retrieve thr private instance y
+        """
         return self.__y
-
+    """
+    sets the private instance y
+    """
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
-        self.__y = value
-
-    def validate_integer(self, name, value, eq=True):
-        '''Method for validating the value.'''
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if eq and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-        elif not eq and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-
+        """
+        sets the private instance y to value
+        """
+        self.validator('y', value)
+        """
+        returns the area value of the Rectangle instance.
+        """
     def area(self):
-        '''Computes area of this rectangle.'''
-        return self.width * self.height
-
+        """
+        returns the area value of the Rectangle instance.
+        """
+        return self.__width * self.__height
+    """
+    model that prints in stdout the Rectangle
+    instance with the character #
+    """
     def display(self):
-        '''Prints string representation of this rectangle.'''
-        s = '\n' * self.y + \
-            (' ' * self.x + '#' * self.width + '\n') * self.height
-        print(s, end='')
-
+        """
+        prints in stdout the Rectangle instance
+        """
+        if self.__width is not None or self.__height is not None:
+            for _ in range(self.__y):
+                print()
+            for _ in range(self.__height):
+                print(' ' * self.__x + '#' * self.__width)
+        """
+        __str__ to provide a string representation of the
+        object and it enhances readability and represents your objects
+        as strings and enables customization to include specific information
+        that is relevant to the class
+        """
     def __str__(self):
-        '''Returns string info about this rectangle.'''
-        return '[{}] ({}) {}/{} - {}/{}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width,
-                   self.height)
-
-    def __update(self, id=None, width=None, height=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
-        if id is not None:
-            self.id = id
-        if width is not None:
-            self.width = width
-        if height is not None:
-            self.height = height
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
+        """
+        __string representation of the object
+        """
+        return (
+                f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - '
+                f'{self.__width}/{self.__height}'
+                )
+        """
+        assigns an argument to each attribute:
+        Update the class Rectangle by adding the public method
+        """
     def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
-        # print(args, kwargs)
+        """
+        assigns an argument to each attribute
+        """
         if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
-
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        else:
+            if 'width' in kwargs:
+                self.__width = kwargs['width']
+            if 'height' in kwargs:
+                self.__height = kwargs['height']
+            if 'x' in kwargs:
+                self.__x = kwargs['x']
+            if 'y' in kwargs:
+                self.__y = kwargs['y']
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+        """
+        Rectangle instance to dictionary representation
+        """
     def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
-        return {"id": self.id, "width": self.__width, "height": self.__height,
-                "x": self.__x, "y": self.__y}
+        """
+        converts to JSON format
+        """
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+            }
