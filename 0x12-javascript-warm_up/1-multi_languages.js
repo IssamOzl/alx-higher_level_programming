ccf288189ace206bce31819650f8260c616fa2eb
IO <issam_ouzali@outlook.fr>
2024-04-09 12:16:24 +0000

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-// It prints 3 lines
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+const cFun = 'C is fun';
+const pyCool = 'Python is cool';
+const jAmaze = 'JavaScript is amazing';
+
+console.log(cFun);
+console.log(pyCool);
+console.log(jAmaze);
